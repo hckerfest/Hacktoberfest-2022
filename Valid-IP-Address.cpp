e45639d4dc2194b21cd40ffc97cb71ba30458bf2
Krishna_Dahale <krishnadahale09@gmail.com>
2022-10-09 13:57:15 +0530

    Update Valid-IP-Address.cpp

--- Valid-IP-Address.cpp
+++ Valid-IP-Address.cpp
@@ -1,12 +1,15 @@
- string validIPAddress(string queryIP) {
-        
-        regex ipv4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])"),ipv6("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");
-        
-        if(regex_match(queryIP,ipv4))
-            return "IPv4";
-        
-        else if(regex_match(queryIP,ipv6))
-            return "IPv6";
-        
-        return "Neither";
+import org.springframework.data.domain;
+
+public class StringCheck{
+    public StringCheck(){
+        ipv4 = ("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+        ipv6 = ("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");
+       if( ExampleMatcher.StringMatcher(ipv4))
+         return ipv4;
+        else
+        return ipv6;
     }
+    public sttaic void main(String args[]){
+        new StringCheck();
+    }
+}
