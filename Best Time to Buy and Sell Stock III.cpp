f11d2d46de586a9c43441fbd08ec8654e5528586
Panav Sharma <107950579+kev00lar@users.noreply.github.com>
2022-10-09 13:52:37 +0530

    Update Best Time to Buy and Sell Stock III.cpp

--- Best Time to Buy and Sell Stock III.cpp
+++ Best Time to Buy and Sell Stock III.cpp
@@ -1,45 +1,9 @@
- int solve(vector<int>&prices, int day, int transactionsLeft, vector<vector<int>> &Memo){
-        
-        if(day == prices.size()){
-            return 0;
-        }
-        
-        if(transactionsLeft == 0){
-            return 0;
-        }
-        
-        int &ans = Memo[day][transactionsLeft]; 
-        
-        if(ans != -1){ // if problem has already been solved 
-            return ans;
-        }
-        
-        // choice 1
-        // no transaction today
-        int ans1 = solve(prices, day + 1, transactionsLeft, Memo);
-        
-        
-        // choice 2
-        // doing the possible transaction today     
-        int ans2 = 0;
-        bool buy = (transactionsLeft % 2 == 0);
-        
-        if(buy == true){ // buy
-            ans2 = -prices[day] + solve(prices, day + 1, transactionsLeft - 1, Memo);
-        }else{ // sell
-            ans2 = prices[day] + solve(prices, day + 1, transactionsLeft - 1, Memo);
-        }
-        
-        return ans = max(ans1, ans2); // store ans in memo before returning
-        
-        
-    }
-    
-    
-    int maxProfit(vector<int>& prices) {
-        
-        vector<vector<int>> Memo(prices.size(), vector<int>(5, -1));
-        int ans = solve(prices, 0, 4, Memo);
-        return ans;
-        
-    }
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy, sell = [inf]*2, [0]*2
+        for x in prices:
+            for i in range(2): 
+                if i: buy[i] = min(buy[i], x - sell[i-1])
+                else: buy[i] = min(buy[i], x)
+                sell[i] = max(sell[i], x - buy[i])
+        return sell[1]
